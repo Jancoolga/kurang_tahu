e292947a63f79a537b7d26dd08d99a67f437bdf8
Jancoolga <111194853+Jancoolga@users.noreply.github.com>
2022-09-23 18:11:16 +0700

    commit kurang_tahu
    ini adalah tugas dari pak ahmad farisi

---
+++ surat ekskul revisi.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+	char y;
+	cout<<"mau buka (program tidak sempurna karena pengembang bingung jadi pls pencet y T-T)\n"; 
+	cin>>y;
+	cout<<"\n";
+	cout<<"***********************************************************************************************************\n";
+	cout<<"kepada sahabat ku\n";
+	cout<<"ricky...\n\n";
+	cout<<"apakah kau baik-baik saja?\n\n";
+	cout<<"aku harap kau baik baik saja\n";
+	cout<<"oh iya,apakah kau masih ingat waktu kita dulu nongkrong bersama teman_teman kita.\n";
+	cout<<"kita tertawa bersama, bermain bersama, sampai memamerkan sihir baru.\n";
+	cout<<"dan aku ingin mengulang kenangan tersebut, jadi aku menundang semua teman teman nongkrong kita.\n";
+	cout<<"termasuk kamu.\n";
+	cout<<"aku harap kau datang ke tempat ku.\n";
+	cout<<"kalau tidak, akan ledakan rumah mu mengunakan sihirku.\n\n";
+	cout<<"salam\n";
+	cout<<"vinlie\n";
+	cout<<"***********************************************************************************************************\n";
+	
+	
+	
+	
+	
+	return 0;
+}
